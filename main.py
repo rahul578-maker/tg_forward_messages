de5964e5c8e77a0721fe5b7ab153193c62a2b1b6
rahul578-maker <130317355+rahul578-maker@users.noreply.github.com>
2023-04-10 03:44:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import asyncio
 
 
 # create your id and hash here: https://my.telegram.org/auth
-api_id = 1111111
-api_hash = '1111b1111111a111111111b1111'
+api_id = 626980225
+api_hash = 'AAGrK9eQmT4-Ay8b4Qv_NN9MtG7_KRjlYy0'
 
 # ID of private group in telegram, find out here: @username_to_id_bot
-chats_id = [-1000000000001, -1000000000002]
-# ID of your channel, find out here: @username_to_id_bot
-my_channel_id = -1000000000003
+chats_id = [-1798894870, -1798894870]
+# ID of your channel, find out here: @Xx12Xx_Bot
+my_channel_id = -1904619787
 
 # Topic ID from 1st group
 topic_id_1 = 1000
@@ -18,7 +18,7 @@ topic_id_1 = 1000
 topic_id_2 = 1000
 
 client = TelegramClient('python_bot', api_id, api_hash)
-client.start(password=input('Enter your 2FA password: '))
+client.start(password=input('Enter your 2FA password: 1 '))
 
 @client.on(events.NewMessage(chats=chats_id))
 async def main(event):
